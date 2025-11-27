10408eb1653032ecadfa9dee8b0557335a9866a8
Daniel Parra <danielestebanparraruiz22@gmail.com>
2025-11-26 20:53:53 -0500

    Creacion del componente header para su reutilizacion en el codigo

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,9 +1,10 @@
 import Header from '../../components/Header/Header.jsx'
-
+import './Landing.css'
 function Landing() {
     return (
         <>
             <Header></Header>
+            <div></div>
         </>
     )
 }
