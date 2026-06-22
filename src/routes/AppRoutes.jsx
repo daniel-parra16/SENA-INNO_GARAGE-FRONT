c3bacc34d880e509249476b7e63b6b34834aaf6a
daniel-parra16 <danielestebanparraruiz22@gmail.com>
2026-06-22 15:24:50 -0500

    Creacion de dockerfile y configuracion para despliegue por medio de imagen

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,16 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import MainLayout from '../components/layout/MainLayout';
-import LoginView from '../features/auth/Index';
+import LoginView from '../features/auth/Index.jsx';
 import RegisterView from '../features/auth/RegisterView';
 import RememberView from '../features/auth/RememberView';
-import DashboardView from '../features/dashboard/Index';
-import OrdenesView from '../features/ordenes/index';
-import UsuariosView from '../features/usuarios/Index';
+import DashboardView from '../features/dashboard/Index.jsx';
+import OrdenesView from '../features/ordenes/Index';
+import UsuariosView from '../features/usuarios/Index.jsx';
 import { useAuth } from '../store/authContext.jsx';
 import VerifyEmailView from '../features/auth/VerifyEmailView.jsx';
 import NewPasswordView from '../features/auth/NewPasswordView.jsx';
-import VehiculosView from '../features/vehiculos/index.jsx';
-import InventarioView from '../features/inventario/index.jsx';
+import VehiculosView from '../features/vehiculos/Index.jsx';
+import InventarioView from '../features/inventario/Index.jsx';
 
 // Permite que solo accedan los usuarios no logueados.
 function GuestRoute({ children }) {
